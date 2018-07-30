042b34f0d46552f828b7b6b4ee94938461e1ddf9
kogotti@yahoo.com <kogotti@yahoo.com>
2018-07-30 18:20:46 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -5,8 +5,8 @@ function init() {
 const body = document.querySelector('body')
 
 body.addEventListener('keydown', function(event){
-  if (code === 38){
-  alert('Hurray');}
+
+  alert('Hurray');
 })
 }
 init()
